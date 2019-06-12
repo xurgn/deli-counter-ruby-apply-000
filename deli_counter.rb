d5e1b466fe01daf3b1ffb3edd62d3d001db92734
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:56:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ end
 def now_serving(arr) 
   if arr.length > 0
     puts "Currently serving " + arr[0] + "."
+    return arr.shift
   else  
     puts "There is nobody waiting to be served!"
   end 
