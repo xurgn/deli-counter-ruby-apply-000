06347d38e91b54d201eb4ea8392ed999fd835153
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:38:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli) 
-  if katz_deli.size > 0 
+  if katz_deli.length > 0 
     puts "The line is currently: 1. Ada 2. Grace 3. Kent"
   end 
   puts "The line is currently empty."
@@ -9,9 +9,10 @@ end
 
 def take_a_number(katz_deli, name) 
   katz_deli << name
-  puts "Welcome, " + name +" You are number " + katz_deli. + " in line."
+  puts "Welcome, " + name +" You are number " + katz_deli.length + " in line."
 end 
 
 def now_serving(katz_deli) 
+  if katz_deli.length > 0
   puts "There is nobody waiting to be served!"
 end 
