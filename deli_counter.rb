caf2ff8755159dc085e7df696078a36834f93de9
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:37:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,8 @@ end
 
 
 def take_a_number(katz_deli, name) 
-  katz_deli.push(name)
-  puts "Welcome, " + name +" You are number " + position + " in line."
+  katz_deli << name
+  puts "Welcome, " + name +" You are number " + katz_deli. + " in line."
 end 
 
 def now_serving(katz_deli) 
