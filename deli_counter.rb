6b69ccbf6f298950c7e91505f431a87494effca2
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:40:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli) 
   if katz_deli.length > 0 
-    name_list.each
+    katz_deli.each_with_index()
      "The line is currently: " + name_list
   end 
   puts "The line is currently empty."
