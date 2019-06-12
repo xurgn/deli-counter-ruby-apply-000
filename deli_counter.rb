d96aa3d3eca2d19d8eb5aafc7be562eff055a330
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:50:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,9 @@ def take_a_number(arr, name)
   puts "Welcome, " + name +" You are number " + arr.length + " in line."
 end 
 
-def now_serving(katz_deli) 
-  if katz_deli.length > 0
-    return "Currently serving " + katz_deli[0]
+def now_serving(arr) 
+  if arr.length > 0
+    return "Currently serving " + arr[0]
   end 
   puts "There is nobody waiting to be served!"
 end 
