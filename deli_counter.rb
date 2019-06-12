583ec5e83021ae08aad7a9fd3a9315f7651019b5
Gina Hsu <regina.xu1@gmail.com>
2019-06-12 04:49:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,8 @@ def line(katz_deli)
 end 
 
 
-def take_a_number(katz_deli, name) 
-  katz_deli << name
+def take_a_number(arr, name) 
+  arr = arr << name
   puts "Welcome, " + name +" You are number " + katz_deli.length + " in line."
 end 
 
